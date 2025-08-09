c5a4a125395457def9a86a89267032da7902dde3
Justin Kian Romeon <romeonjustin@gmail.com>
2025-08-09 09:47:01 +0800

    feat: create and add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css'
+import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      <div>
+        <Link to='/'>Cinemarker</Link>
+      </div>
+
+      <div>
+        <Link to='/'>Home</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
