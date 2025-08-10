7d06f2779f6cf36105271392c1aa5d25102b6c15
Justin Kian Romeon <romeonjustin@gmail.com>
2025-08-11 07:26:02 +0800

    feat: add typesafety to api functions

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,30 @@
+import type { Movie, RawTMDBMovie } from "./types";
+
 const BASE_URL: string = import.meta.env.VITE_;
 const API_KEY: string = import.meta.env.VITE_API_KEY;
 
-export async function popularMovies() {
+export async function popularMovies(): Promise<Movie[]> {
+
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
-  return data.results;
+
+  return data.results.map((raw: RawTMDBMovie) => ({
+    id: raw.id,
+    url: `https://image.tmdb.org/t/p/w500${raw.poster_path}`,
+    title: raw.title,
+    release_date: raw.release_date
+  }));
 }
 
-export async function SearchMovies(searchTerm:string) {
+export async function SearchMovies(searchTerm:string): Promise<Movie[]> {
+
   const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchTerm}`);    
   const data = await response.json();
-  return data.results;
+
+  return data.results.map((raw: RawTMDBMovie) => ({
+    id: raw.id,
+    url: `https://image.tmdb.org/t/p/w500${raw.poster_path}`,
+    title: raw.title,
+    release_date: raw.release_date
+  }));
 }
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,20 @@ export interface Movie {
   title: string,
   release_date: string
 }
+
+export interface RawTMDBMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
